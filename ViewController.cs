b45fb432032c689183845648a8b26dc4d6502ac9
Mark <sirkenix@gmail.com>
2015-05-14 13:51:03 +0200

    Initial commit

---
+++ ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+
+namespace HideKeyboard
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			EntryTextField.ShouldReturn = (textField) => {
+				textField.ResignFirstResponder ();
+				return true;
+			};
+
+			var g = new UITapGestureRecognizer (() => View.EndEditing (true));
+			g.CancelsTouchesInView = false; //for iOS5
+			View.AddGestureRecognizer (g);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
